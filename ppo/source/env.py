947c66536ec1ad33490c1f3006e3a2a05d1a960c
Jingxi Chen <42823098+codingrex@users.noreply.github.com>
2020-05-25 17:33:49 -0400

    adopt change in env.py
    Adopt new version of pre-computed visbility in order to save the run time

--- ppo/source/env.py
+++ ppo/source/env.py
@@ -26,10 +26,30 @@ class Env:
         self.timeStep = CONST.TIME_STEP
         self.obsMaps, self.vsbs, self.vsbPolys = self.initObsMaps_Vsbs()
         self.obstacleMap , self.vsb, self.vsbPoly, self.mapId = self.setRandMap_vsb()
+        # modified: decay rate:
+        self.decay= 2
+        # modified: cap the upperbound of penalty
+        self.cap= 200
+
+
+
+
+        self.obstacleMap , self.vsb, self.mapId = self.setRandMap_vsb()
         self.obstacleMap,self.obsPlusViewed, self.currentMapState, self.agents, self.adversaries = self.initTotalArea_agents(CONST.NUM_AGENTS)
-        self.prevUnviewedCount = np.count_nonzero(self.currentMapState==0)
+
+        # modified: reward map
+        #self.rewardMap= np.where(self.obstacleMap == 0, 0, self.obstacleMap)
+
+
+        #modified: previous sum of reward
+        #self.prevSumR = np.sum(self.rewardMap)
+
+
+
         self.fourcc = cv2.VideoWriter_fourcc(*'mp4v')
         self.out = cv2.VideoWriter(f"checkpoints/cnn1.avi",self.fourcc, 50, (700,700))
+        self.out_test= cv2.VideoWriter(f"./testVideo.avi",self.fourcc, 50, (700,700))
+
     
     def initObsMaps_Vsbs(self):
         return obsMap.getAllObs_vsbs(np.zeros((50,50)))
@@ -68,10 +88,14 @@ class Env:
         agentPos = [agent.getState()[0] for agent in agents]
         gPos = self.cartesian2Grid(agentPos)
         temp = self.updatePosMap(gPos, obstacleViewedMap, 100)
-        
+
+        currentMapState= temp
+
+        """ Adversary positoin 
         advrsyPos = [adversary.getState()[0] for adversary in adversaries]
         advPos = self.cartesian2Grid(advrsyPos)
         currentMapState = self.updatePosMap(advPos, temp, 200)
+        """
         
         return obstacleMap, obstacleViewedMap, currentMapState, agents, adversaries
     
@@ -84,10 +108,17 @@ class Env:
         agentPos = [agent.getState()[0] for agent in self.agents]
         gPos = self.cartesian2Grid(agentPos)
         temp = self.updatePosMap(gPos, obstacleViewedMap, 100)
-        
+
+        currentMapState= temp
+
+
+        #adversary position
+        """ 
         advrsyPos = [adversary.getState()[0] for adversary in self.adversaries]
         advPos = self.cartesian2Grid(advrsyPos)
         currentMapState = self.updatePosMap(advPos, temp, 200)
+        
+        """
         
         return obstacleMap, obstacleViewedMap, currentMapState
     
@@ -103,13 +134,15 @@ class Env:
         self.obstacleMap , self.vsb, self.vsbPoly, self.mapId = self.setRandMap_vsb()
         self.obstacleMap,self.obsPlusViewed, self.currentMapState, self.agents, self.adversaries = self.initTotalArea_agents(CONST.NUM_AGENTS)
 
-        self.prevUnviewedCount = np.count_nonzero(self.currentMapState==0)
-        
-        advrsyPos = [adversary.getState()[0] for adversary in self.adversaries]
+        # modified: reward map
+        #self.rewardMap = np.where(self.obstacleMap == 0, 0, self.obstacleMap)
+
+        # modified: previous sum of reward
+        #self.prevSumR = np.sum(self.rewardMap)
         
         state = []
         for agent in self.agents:
-            state.append([agent.getState()[0], advrsyPos, self.currentMapState])
+            state.append([agent.getState()[0],self.currentMapState])
         
         return state
         
@@ -169,64 +202,160 @@ class Env:
         gPos = self.cartesian2Grid(agentPos)
         # get new visibility and update obsPlusViewed
         self.obsPlusViewed = self.vsb.updateVsbOnImg(agentPos,self.obsPlusViewed, self.vsbPoly)
+
+        #Genearete currentStateMap with decay reward:
+
+        # 150 is the wall, 255 (->0) (is newly viewed), initial unviewed is 0,  all pixels except wall is < 0, agent 100
+
+        #update the new current viewed area from updateVsbPloygon set current viewed to 255
+        self.currentMapState= self.vsb.updateVsbPolyOnImg(agentPos,self.currentMapState)
+
+        # If pixel <= 0 (not wall (150), agent (100), current viewed (255->0)) decrease by decay rate
+        self.currentMapState = np.where((self.currentMapState <= 0), self.currentMapState - self.decay, self.currentMapState)
+
+
+        # set current under viewed pixels(255) to 0, after decay complete
+        self.currentMapState= np.where((self.currentMapState == 255), 0, self.currentMapState)
+
+        # apply the lowerbound cap for the penalty
+        self.currentMapState = np.where((self.currentMapState < (-1 * self.cap)), -1 * self.cap, self.currentMapState)
+
+
         # update position on currentMapState
-        temp = self.updatePosMap(gPos, self.obsPlusViewed, 100)
-        
+        self.currentMapState= self.updatePosMap(gPos, self.currentMapState, 100)
+
+
+        #Adversary, left unused for now
+        """
         advrsyPos = [adversary.getState()[0] for adversary in self.adversaries]
         advPos = self.cartesian2Grid(advrsyPos)
         self.currentMapState = self.updatePosMap(advPos, temp, 200)
+
         
-#        AdvVisibility = self.vsb.checkPtInVsbPolyDict(advrsyPos, agentPos, self.vsbPoly)
-        
-        separation = np.linalg.norm(advrsyPos[0]-agentPos[0])
-        AdvVisibility = separation <= CONST.SEPERATION_PENALTY
+        AdvVisibility = self.vsb.checkPtInVsbPoly(advrsyPos, agentPos)
+        """
+
+        #currently set adversary visibility to false : does't consider
+        AdvVisibility= False
         
         display = self.currentMapState
         # update reward mechanism
-        newAreaVis, penalty = self.getReward(AdvVisibility)
-        reward = newAreaVis + penalty
-        done = np.count_nonzero(self.currentMapState==0) == 0
-        return agentPos, advrsyPos, display, reward, newAreaVis, penalty, done
-                
+        sumR, penalty = self.getReward(AdvVisibility)
+        reward = sumR + penalty
+        #done = np.count_nonzero(self.currentMapState==0) == 0
+        done= False
+        return agentPos, display, reward, sumR, penalty, done
 
-    def render(self):
+    def render(self, show = 0):
+        cap= self.cap
+
         img = np.copy(self.currentMapState)
-        img = np.rot90(img,1)
-        r = np.where(img==150, 255, 0)
-        g = np.where(img==100, 255, 0)
-        
-        b = np.zeros_like(img)
-        b_n = np.where(img==255, 100, 0)
-        bgr = np.stack((b,g,r),axis = 2)
-        bgr[:,:,0] = b_n
-        
-        # adversary Pos
-        advPos = np.where(img == 200)
-        bgr[advPos[0], advPos[1],1] = 255
-        bgr[advPos[0], advPos[1],2] = 255
-        
-        displayImg = cv2.resize(bgr,(700,700),interpolation = cv2.INTER_AREA)
-        
-        cv2.imshow("Position Map", displayImg)
-        cv2.waitKey(1)
-    
+
+        reward_map = img
+
+
+
+        """ initialize heatmap """
+        heatmap = cv2.resize(reward_map, (700, 700), interpolation=cv2.INTER_AREA)
+        heatmapshow = np.rot90(heatmap, 1)
+
+        heatmapshow = np.where(heatmapshow == 150 , 20, heatmapshow)
+        heatmapshow = np.where(heatmapshow < 0, -1 * heatmapshow * 255 / cap, -1 * heatmapshow)
+        heatmapshow = np.where(heatmapshow >= 200, 255, heatmapshow)
+
+
+
+
+        heatmapshow = heatmapshow.astype(np.uint8)
+
+
+
+        heatmapshow = cv2.applyColorMap(heatmapshow, cv2.COLORMAP_JET)
+
+        if (show == 1):
+            cv2.imshow("Heatmap", heatmapshow)
+            cv2.waitKey(1)
+
+        return  heatmapshow
+
+
+
+    def parse_action(self, action):
+        if action == 0:
+            return 'stay'
+        elif action == 1:
+            return 'up'
+        elif action == 2:
+            return 'left'
+        elif action == 3:
+            return 'down'
+        else:
+            return 'right'
+
+
+    def render_test(self, step, aActions, episode):
+        self.generate_heat_text(aActions, step, episode, 1)
+
+    def save_test(self, step, aActions, episode):
+
+
+        displayImg = self.generate_heat_text(aActions, step, episode)
+
+        self.out_test.write(displayImg.astype('uint8'))
+
+
+
+
+
+    def generate_heat_text(self,  action, count, episode, show= 0):
+        """ initialize heatmap """
+        heatmapshow = self.render()
+
+        # font
+        font = cv2.FONT_HERSHEY_SIMPLEX
+
+        # org
+        org2 = (450, 15)
+        org1 = (10, 15)
+        org4 = (10, 45)
+        org3 = (450, 45)
+
+        # fontScale
+        fontScale = 0.6
+
+        # Blue color in BGR
+        color = (0, 255, 0)
+
+        # Line thickness of 2 px
+        thickness = 2
+
+        reward, penalty = self.getReward(False)
+
+        # Using cv2.putText() method
+
+        heatmapshow = cv2.putText(heatmapshow, "current episode: " + str(episode), org1, font,
+                                  fontScale, color, thickness, cv2.LINE_AA)
+
+        heatmapshow = cv2.putText(heatmapshow, "current step: " + str(count), org4, font,
+                                  fontScale, color, thickness, cv2.LINE_AA)
+
+        heatmapshow = cv2.putText(heatmapshow, "total penalty: " + str(reward + penalty), org2, font,
+                                  fontScale, color, thickness, cv2.LINE_AA)
+
+        word = self.parse_action(action)
+        heatmapshow = cv2.putText(heatmapshow, "action: " + word, org3, font,
+                                  fontScale, color, thickness, cv2.LINE_AA)
+
+        if(show == 1):
+            cv2.imshow("Heatmap for testLoop", heatmapshow)
+            cv2.waitKey(1)
+
+        return heatmapshow
+
     def save2Vid(self):
-        img = np.copy(self.currentMapState)
-        img = np.rot90(img,1)
-        r = np.where(img==150, 255, 0)
-        g = np.where(img==100, 255, 0)
+
         
-        b = np.zeros_like(img)
-        b_n = np.where(img==255, 100, 0)
-        bgr = np.stack((b,g,r),axis = 2)
-        bgr[:,:,0] = b_n
-        
-        # adversary Pos
-        advPos = np.where(img == 200)
-        bgr[advPos[0], advPos[1],1] = 255
-        bgr[advPos[0], advPos[1],2] = 255
-        
-        displayImg = cv2.resize(bgr,(700,700),interpolation = cv2.INTER_AREA)
+        displayImg = self.render()
         
         self.out.write(displayImg.astype('uint8'))
 #        cv2.imshow("raw", displayImg)
@@ -237,24 +366,33 @@ class Env:
         for pos in gPos:
             currMapState[pos[0],pos[1]] = val
         return currMapState
-        
-        
+
+
     def getReward(self, AdvVisibility):
-        curUnviewedCount = np.count_nonzero(self.currentMapState==0)
-        newAreaVis = self.prevUnviewedCount - curUnviewedCount
-        self.prevUnviewedCount = curUnviewedCount
-        
-        if newAreaVis < 0:
-            print("Error calculating newArea")
-        
+        #150 is the wall, 255 (->0) (is newly viewed), initial unviewed is 0,  all pixels except wall is < 0, agent 100
+
+        #sum up reward on all free pixels
+        actualR = np.where((self.currentMapState<= 0), self.currentMapState, 0)
+        curSumR = np.sum(actualR)
+
+
         penalty = 0
         if AdvVisibility:
-            penalty += -100
-#            print("Visible")
+            penalty += 0
+        #            print("Visible")
         else:
-#            print("Not Visible")
+            #            print("Not Visible")
             pass
-        return newAreaVis, penalty
+
+        return curSumR, penalty
+
+
+        
+
+
+
+
+
         
     def cartesian2Grid(self, posList):
         gridList = []
